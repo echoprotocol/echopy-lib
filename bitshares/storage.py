354244dfabbed877b29a2c96f4369a4c41e42270
Evgenii Vasilyev <e.vasilev@pixelplex.io>
2019-01-22 15:14:48 +0300

    Fix url and storage config

--- bitshares/storage.py
+++ bitshares/storage.py
@@ -9,18 +9,18 @@ from graphenestorage import (
 )
 
 
-url = "wss://node.bitshares.eu"
+url = "ws://195.201.164.54:6311/"
 InRamConfigurationStore.setdefault("node", url)
 SqliteConfigurationStore.setdefault("node", url)
 
 
 def get_default_config_store(*args, **kwargs):
     if "appname" not in kwargs:
-        kwargs["appname"] = "bitshares"
+        kwargs["appname"] = "echo"
     return SqliteConfigurationStore(*args, **kwargs)
 
 
 def get_default_key_store(config, *args, **kwargs):
     if "appname" not in kwargs:
-        kwargs["appname"] = "bitshares"
+        kwargs["appname"] = "echo"
     return SqliteEncryptedKeyStore(config=config, **kwargs)
