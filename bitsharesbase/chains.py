a6b5dc30c432b9b1e2a4363f214ecbc7bba598c1
Evgenii Vasilyev <e.vasilev@pixelplex.io>
2019-01-22 15:12:58 +0300

    Add ECHO chain

--- bitsharesbase/chains.py
+++ bitsharesbase/chains.py
@@ -10,4 +10,9 @@ known_chains = {
         "core_symbol": "TEST",
         "prefix": "TEST",
     },
+    "ECHO": {
+        "chain_id": "65e2ef320cd54dede55dd6867bbd20432eb8a7c957877a63130d753a461b8105",
+        "core_symbol": "ECHO",
+        "prefix": "ECHO",
+    },
 }
