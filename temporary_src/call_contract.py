ef16a234e0b3ed5ca3d1d5f988834ba4c7674efb
y.sidoruk <y.sidoruk@pixelplex.io>
2019-01-23 13:08:18 +0300

    fix

--- temporary_src/call_contract.py
+++ temporary_src/call_contract.py
@@ -1,6 +1,5 @@
 from bitshares import BitShares
 
-# connect to testnet
 echonet = BitShares(
     "ws://195.201.164.54:6311/",
     bundle=True,
@@ -12,8 +11,6 @@ if not echonet.wallet.created():
 
 echonet.wallet.unlock('qwe')
 
-#echonet.create_account(account_name='yarosalv', registrar ="1.2.0", owner_key='ECHO5WeESxRb9ftmjcGYptTSi59ygWWECBbPZSRVjyNang36dcM6XY',
-#   active_key='ECHO5WeESxRb9ftmjcGYptTSi59ygWWECBbPZSRVjyNang36dcM6XY', memo_key='ECHO5WeESxRb9ftmjcGYptTSi59ygWWECBbPZSRVjyNang36dcM6XY')
 
 echonet.call_contract(code="dce4a447000000000000000000000000010000000000000000000000000000000000006a",
                       registrar="1.2.16", callee="1.16.106")
