ab46c31c05ea3beb78073757b13d4a8a96bc6106
e.vasilev <e.vasilev@pixelplex.io>
2019-10-22 16:36:44 +0300

    Up library patch version >> 0.1.44: update registration api

--- echopy/echoapi/ws_api/registration_api.py
+++ echopy/echoapi/ws_api/registration_api.py
@@ -3,9 +3,6 @@ class RegistrationApi:
     def __init__(self, db):
         self.db = db
 
-    def register_account(self, callback, name, active_key, echorand_key):
-        return self.db.rpcexec('register_account', [callback, name, active_key, echorand_key])
-
     def request_registration_task(self):
         return self.db.rpcexec('request_registration_task', [])
 
